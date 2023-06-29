b87789f19a576b7f7e0d26a1cef0882c627efa89
SDMcSweeney <ppxsm5@nottingham.ac.uk>
2023-06-29 21:48:58 +0100

    start of custom policy
    initial changes to begin working on custom sampling etc

--- demos/few_action_baselines/im_demo.py
+++ demos/few_action_baselines/im_demo.py
@@ -4,7 +4,7 @@ import os, sys
 from fix_pathing import root_dir
 
 from src.utils.animation import make_animation_vertical, make_animation_horizontal
-from src.envs.east_model.east_model import EastModel, activity
+from src.envs.ising_model_1d.ising_model_1d import IsingModel, activity
 
 from src.utils.plotting import render_spin_trajectory, plot_learning_curve
 import haiku as hk
@@ -22,8 +22,67 @@ from src.utils.evaluation import (
 
 # %% INIT ENV
 rng = hk.PRNGSequence(123)
-config = {"L": 20, "bias": 0, "d": 2, "render_mode": None, "obs_fn": activity}
-env = EastModel(config)
+config = {"L": 20, "bias": 0, "d": 2, "render_mode": None, "obs_fn": activity, "mean": 0}
+env = IsingModel(config)
+
+
+# %% ISING ATTEMPT 1
+
+def ising_initial_policy(key,state,config):
+    #chance_of_choice = 0
+    #chosen_change= jnp.array(config["L"], dtype=jnp.int64)
+    #return jnp.log(chance_of_choice), (chosen_change, key)
+    key, subkey = jr.split(key)
+    return jnp.log(1 / config["L"]), (
+        jr.randint(subkey, [1], 1, config["L"] + 1, dtype=jnp.int64),
+        key,
+    )
+
+
+policy_isinginitial_jit = jit(partial(ising_initial_policy, config=config))
+
+
+NUM_STEPS = 1000
+start_time = time.time()
+
+(
+    _,
+    _,
+) = env.reset()   #STATE IS INITIALISED
+states_cache = [env.state]
+env_rewards_cache = []
+rewards_cache = []
+activity_cache = []
+current_cache = []
+logp_cache = []
+s_t = env.state
+
+activity_jit = jit(activity)  #activity counts flips
+
+for _ in range(NUM_STEPS):
+
+    logp, (a_t, _) = env.policy_isinginitial_jit(next(rng), s_t)
+
+    logp_cache.append(logp)  # entropy
+
+    s_tp1, r_t, terminated, truncated, info = env.step(a_t)
+
+    env_rewards_cache.append(r_t)
+    rewards_cache.append(r_t - logp)  # subtract entropy term, logp
+
+    states_cache.append(s_tp1)
+    rewards_cache.append(r_t)
+
+    activity_cache.append(activity_jit(s_t, a_t, s_tp1))
+
+    s_t = s_tp1
+
+loop_time = time.time() - start_time
+time_per_step = loop_time / NUM_STEPS
+est_time_full_run = time_per_step * 10**6 / 3600  # time for 1 million steps in hours
+print(f"{loop_time*1000:.2f}", "ms")
+
+
 
 
 # %% EXAMPLE OF TAKING NO ACTIONS EVERY TIME
@@ -49,6 +108,7 @@ logp_cache = []
 s_t = env.state
 
 activity_jit = jit(activity)
+
 
 for t in range(NUM_STEPS):
     logp, (a_t, _) = policy_no_action_jit(None, None)
