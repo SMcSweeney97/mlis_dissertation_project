ab5d254971205d5967bc422adc0d67acb38b9121
Edward <gillman.edward@gmail.com>
2023-07-25 16:18:55 +0100

    added basic a2c and updated mcmc_demo

---
+++ demos/few_action_baselines/a2c_demo_im_lds.py
@@ -0,0 +1,217 @@
+# %% IMPORTS
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', "..", "stat-mech-gym", "src")) #gym
+
+import jax.numpy as jnp
+import haiku as hk
+import matplotlib.pyplot as plt
+import numpy as np
+import coax
+from optax import sgd
+import time
+import optax
+
+import os, sys
+
+from fix_pathing import root_dir
+
+from src.envs.ising_model_1d.ising_model import IsingModel, activity, magnetisation
+
+from src.utils.plotting import plot_learning_curve
+import haiku as hk
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from jax import jit
+import jax.numpy as jnp
+import jax.random as jr
+
+from src.utils.evaluation import (
+    evaluate_policy_biases,
+)
+
+import seaborn as sns
+
+import jax
+# %% INIT RNG AND CONFIG
+seed_env = 567
+seed_pi = 234
+seed_vf = 123
+# %% INIT ENV
+config = {"L": 4, "bias": 0, "d": 2, "D": 2, "temp":0.2, "render_mode": None, "obs_fn": activity}
+env = IsingModel(config, seed=seed_env)
+# %%
+def func_v(S, is_training):
+    value = hk.nets.MLP(output_sizes=[4,1], w_init=jnp.zeros, b_init=jnp.zeros)
+    return jnp.ravel(value(S))
+
+def func_pi(S, is_training, config):
+    logits = hk.nets.MLP(output_sizes=[4,config["L"]**config["D"]+1], w_init=jnp.zeros, b_init=jnp.zeros)
+    return {'logits': logits(S)}
+# %% DEFINE THE HYPER-PARAMS
+BOOTSTRAP_N = 1
+DISCOUNT = 1
+LR_PI = 1e-2
+LR_VF = 1e-2
+LR_R = 1e-3
+tracer = coax.reward_tracing.NStep(n=1, gamma=DISCOUNT) #stores (R_t^n, I_t^n, S_t+n, R_t+n)
+# %% RUN A TEST LOOP WITH ASSERTIONS ETC.
+NUM_STEPS = 10000
+OBS_FREQ = 1
+s_0, _ = env.reset()
+tracer.reset()
+s_0 = env.state
+rewards = []
+reward_diff = []
+states = [s_0]
+s_t = s_0
+rb_t = 0.0 #Estimate of the average-reward per time-step
+avg_rew_ests = []
+actions = []
+env_rewards = []
+entropies = []
+mags = []
+activities = []
+
+pi = coax.Policy(lambda S, is_training: func_pi(S, is_training, config), env, proba_dist=coax.proba_dists.CategoricalDist(env.action_space, gumbel_softmax_tau=0.2), random_seed=seed_pi)
+vf = coax.V(func_v, env, random_seed=seed_vf)
+
+simple_td = coax.td_learning.SimpleTD(vf, vf,  optimizer=optax.adam(learning_rate=LR_VF), loss_function=coax.value_losses.mse) #TD UPDATER
+vanilla_pg = coax.policy_objectives.VanillaPG(pi, optimizer=optax.adam(learning_rate=LR_PI)) # POLICY-GRAD UPDATER
+
+#store r_t, rb_t and states for comparisons
+rewards = []
+avg_reward_ests = [rb_t]
+states = [s_0]
+
+start_time = time.time()
+
+for t in range(NUM_STEPS):
+
+    a_t, logp_t = pi(s_t, return_logp=True)
+    s_tp1, r_t, terminated, truncated, info = env.step(a_t)
+
+    actions.append(a_t)
+    env_rewards.append(r_t)
+    entropies.append(logp_t)
+    mags.append(magnetisation(s_t, a_t, s_tp1))
+    activities.append(activity(s_t, a_t, s_tp1))
+
+    r_t = r_t - logp_t #entropy term
+    rd_t = r_t - rb_t #differential reward
+
+    tracer.add(s_t, a_t, rd_t, terminated, logp=logp_t)
+
+    while tracer:
+
+        transition_batch = tracer.pop()
+
+        metrics_v, td_error = simple_td.update(transition_batch, return_td_error=True)
+        metrics_pi = vanilla_pg.update(transition_batch, td_error) #advantage factor is the td_error for A2C
+
+        rb_t = rb_t + LR_R*td_error.item() #update rb_t
+
+    s_t = s_tp1
+
+
+    if t%OBS_FREQ==0:
+
+        states.append(s_tp1)
+        rewards.append(r_t)
+        avg_reward_ests.append(rb_t)
+
+loop_time = time.time() - start_time
+time_per_step = loop_time/NUM_STEPS
+print(f"{loop_time:.2f}s for {NUM_STEPS} steps")
+
+# %%
+
+rewards = np.ravel(rewards)
+avg_rew_ests = np.ravel(avg_reward_ests)
+
+# %% Plot ESTIMATES OF AVERAGE REWARD
+fig, axs = plt.subplots(1,1, figsize=(10,5))
+axs.plot(np.arange(0, NUM_STEPS + OBS_FREQ, OBS_FREQ), avg_reward_ests, "bs--", linewidth=4, label="rb_t")
+axs.set_xlim(0, NUM_STEPS)
+axs.legend()
+# %% Plot Magnetisation LC
+# for L=4, T = 0.5 (beta = 0.2), |M| approx 0.35 for PBC
+fig = plot_learning_curve(np.ravel(mags), ylabel="Mag")
+
+# %% Histogram of Actions
+sns.displot(actions, discrete=True)
+# %% Frac of no-flip-actions
+no_flip_actions = np.cumsum(np.array(actions) == config["L"]**config["D"])
+frac_no_flip = no_flip_actions/np.arange(1,len(no_flip_actions)+1)
+plt.plot(frac_no_flip)
+num_no_flip_actions = np.sum(np.array(actions) == config["L"]**config["D"])
+plt.title(f"Long-time no change frac = {num_no_flip_actions}/{len(actions)} = {num_no_flip_actions/len(actions):.2f}")
+plt.ylim(0,1)
+# %% Activity
+fig_activity = plot_learning_curve(activities, hlines=[np.mean(activities)])
+plt.ylim(0,1)
+plt.title(f"Long-time no average activity = {np.mean(activities):.2f}")
+# %% Actions
+plt.plot(actions)
+# %% KL-div
+kl_div = -np.ravel(rewards) - config["bias"]*np.ravel(activities)
+fig_kl= plot_learning_curve(kl_div, hlines=[np.mean(kl_div)])
+plt.title(f"MC Approx to KL-div in Steady State = {np.mean(kl_div):.2f}")
+# %% COMPARE WITH TD-EST
+kl_div_est = -np.ravel(avg_reward_ests[1::]) - np.cumsum(config["bias"]*np.ravel(activities))/np.arange(1,len(np.ravel(activities))+1)
+plt.plot(kl_div_est)
+plt.title(f"MC Approx to KL-div in Steady State = {np.mean(kl_div_est[6000::]):.2f}")
+# %% EVALUATE THE POLICY
+# %% RUN A TEST LOOP WITH ASSERTIONS ETC.
+NUM_STEPS = 10000
+OBS_FREQ = 1
+s_0, _ = env.reset()
+tracer.reset()
+s_0 = env.state
+rewards = []
+reward_diff = []
+states = [s_0]
+s_t = s_0
+eval_results = {}
+eval_results["avg_rew_ests"] = []
+actions = []
+env_rewards = []
+entropies = []
+mags = []
+activities = []
+
+#store r_t, rb_t and states for comparisons
+rewards = []
+avg_reward_ests = [rb_t]
+states = [s_0]
+
+start_time = time.time()
+
+for t in range(NUM_STEPS):
+
+    a_t, logp_t = pi(s_t, return_logp=True)
+    s_tp1, r_t, terminated, truncated, info = env.step(a_t)
+
+    actions.append(a_t)
+    env_rewards.append(r_t)
+    entropies.append(logp_t)
+    mags.append(magnetisation(s_t, a_t, s_tp1))
+    activities.append(activity(s_t, a_t, s_tp1))
+
+    r_t = r_t - logp_t #entropy term
+
+    s_t = s_tp1
+
+    if t%OBS_FREQ==0:
+
+        states.append(s_tp1)
+        rewards.append(r_t)
+
+loop_time = time.time() - start_time
+time_per_step = loop_time/NUM_STEPS
+print(f"{loop_time:.2f}s for {NUM_STEPS} steps")
+# %% Plot ESTIMATES OF AVERAGE REWARD
+fig = plot_learning_curve(rewards)
+# %%
+sns.displot(actions, discrete=True)
+# %%
--- demos/few_action_baselines/im_mcmc_demo.py
+++ demos/few_action_baselines/im_mcmc_demo.py
@@ -4,7 +4,7 @@ import os, sys
 from fix_pathing import root_dir
 
 from src.utils.animation import make_animation_vertical, make_animation_horizontal
-from src.envs.ising_model_1d.ising_model import IsingModel, activity, magnetisation, get_possible_states, random_initial_state, logp_state_proposal, logp_acceptance, logp_ref
+from src.envs.ising_model_1d.ising_model import IsingModel, activity, magnetisation, get_possible_states, logp_state_proposal_vmapped, log_ratio_target_dist, logp_acceptance_vmapped
 
 from src.utils.plotting import render_spin_trajectory, plot_learning_curve
 import haiku as hk
@@ -20,12 +20,68 @@ from jax import vmap
 from src.utils.evaluation import (
     evaluate_policy_biases,
 )
-
+import seaborn as sns
 # %% INIT ENV
 rng = hk.PRNGSequence(456)
-config = {"L": 20, "bias": 0, "d": 2, "D":2,"temp":0.2, "render_mode": None, "obs_fn": activity, "mean": 0}
-env = IsingModel(config)
-# %% ISING ATTEMPT 1
+env_seed = 123
+config = {"L": 8, "bias": 0, "d": 2, "D":2,"temp":0.2, "render_mode": None, "obs_fn": activity, "mean": 0}
+env = IsingModel(config, seed=env_seed)
+# %% ISING - EXPLICIT TEST OF MCMC STEP FOR DIAGNOSTICS
+
+NUM_STEPS = 1
+start_time = time.time()
+
+(
+    _,
+    _,
+) = env.reset()   #STATE IS INITIALISED
+states_cache = [env.state]
+env_rewards_cache = []
+rewards_cache = []
+activity_cache = []
+magnetisation_cache = []
+actions_cache = []
+current_cache = []
+logp_cache = []
+s_t = env.state
+
+activity_jit = jit(activity)  #activity counts flips
+magnetisation_jit = jit(magnetisation)
+
+for t in range(NUM_STEPS):
+
+    # print(t)
+
+    # TO CHECK - ENERGY DIFFERENCE SEEM VERY LARGE?
+    possible_states = get_possible_states(s_t, config)
+    energy_diffs = vmap(lambda proposed_state: log_ratio_target_dist(s_t, proposed_state, config))(possible_states)
+    proposal_probs = jnp.exp(logp_state_proposal_vmapped(s_t, possible_states, config))
+    acceptance_probs = jnp.exp(logp_acceptance_vmapped(s_t, possible_states, config))
+
+
+    logp, (a_t, _) = env.policy_ref_jit(next(rng), s_t)
+
+    actions_cache.append(a_t)
+    logp_cache.append(logp)  # entropy
+
+    s_tp1, r_t, terminated, truncated, info = env.step(a_t)
+
+    # print("...", logp, r_t, a_t[0])
+
+    env_rewards_cache.append(r_t) #log[q] + r_obs
+    rewards_cache.append(r_t - logp)  # subtract entropy term, logp
+
+    states_cache.append(s_tp1)
+    activity_cache.append(activity_jit(s_t, a_t, s_tp1))
+    magnetisation_cache.append(magnetisation_jit(s_t, a_t, s_tp1))
+
+    # s_t = s_tp1
+
+loop_time = time.time() - start_time
+time_per_step = loop_time / NUM_STEPS
+est_time_full_run = time_per_step * 10**6 / 3600  # time for 1 million steps in hours
+print(f"{loop_time*1000:.2f}", "ms")
+# %% ISING
 
 NUM_STEPS = 10000
 start_time = time.time()
@@ -103,4 +159,14 @@ fig_current = plot_learning_curve(activity_cache, ylabel=r"$<\mathrm{activity}>_
 
 print("rb = ", np.mean(rewards_cache) - np.mean(logp_cache))
 print(r"<\kappa> = ", np.mean(activity_cache))
+# %% Histogram of Actions
+actions_cache = np.ravel(actions_cache)
+sns.displot(actions_cache, discrete=True)
+# %% Frac of no-flip-actions
+no_flip_actions = np.cumsum(np.array(actions_cache) == config["L"]**config["D"])
+frac_no_flip = no_flip_actions/np.arange(1,len(no_flip_actions)+1)
+plt.plot(frac_no_flip)
+num_no_flip_actions = np.sum(np.array(actions_cache) == config["L"]**config["D"])
+plt.title(f"Long-time no change frac = {num_no_flip_actions}/{len(actions_cache)} = {num_no_flip_actions/len(actions_cache):.2f}")
+plt.ylim(0,1)
 # %%
